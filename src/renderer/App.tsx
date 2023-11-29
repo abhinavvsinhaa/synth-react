41e5fe2ab881eae896b1dc5554c6cea65b31608b
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2023-11-29 09:07:05 +0530

    Electron with React and Typescript

---
+++ src/renderer/App.tsx
@@ -0,0 +1,20 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+function Hello() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
