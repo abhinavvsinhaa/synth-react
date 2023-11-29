18a0c4c578a11736fe9a53791518181d405a675c
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2023-11-29 10:09:46 +0530

    ct window stylings

--- src/renderer/views/sidebar/ct/CT.tsx
+++ src/renderer/views/sidebar/ct/CT.tsx
@@ -1,9 +1,22 @@
 import '../../../css/views/ct.css'
 
+const Compile = () => {
+  return (
+    <div>
+      <div className={'ct-header'}>
+        <div className={'tab-details'}>
+          <p className={'tab-name'}>Compile</p>
+          <p className={'tab-description'}>See your code's compilation results here</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export const CT = () => {
   return (
     <div className={'ct'}>
-      <p>CT</p>
+      <Compile/>
     </div>
   );
 }
